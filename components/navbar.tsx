4d6f90daf4593b9bbd20ebfbd2ad3ce5dddb728b
Nick Harris <nharris614@gmail.com>
2022-01-17 11:08:44 -0600

    Add layout

---
+++ components/navbar.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Navbar() {
+  return (
+    <>
+      {/* top section */}
+      <div className={styles.titleSummaryContainer}>
+        <h1>
+          Nick Harris
+        </h1>
+        <p>
+          Web developer | Musician
+        </p>
+        <p>
+          Chicago, Illinois
+        </p>
+      </div>
+
+      {/* nav */}
+      <nav className={styles.nav}>
+        {/* projects */}
+        <a href="/">Projects</a>
+        {/* discography */}
+        <Link href="/discography">
+          <a>Discography</a>  
+        </Link>
+        {/* contact */}
+        <a href="/">Contact</a>
+      </nav>
+    </>
+  )
+}
