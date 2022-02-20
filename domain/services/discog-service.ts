5d54e6d95677c96ddf72ffa0b084ad84661434c0
Nick Harris <nharris614@gmail.com>
2022-02-20 01:38:05 -0600

    Take out comments

--- domain/services/discog-service.ts
+++ domain/services/discog-service.ts
@@ -5,39 +5,13 @@ const httpClient = new HttpClient();
 
 export class DiscogService {
 
-  // const nhReleaseData = getArtistReleaseData("6067515");
-  // const rmReleaseData = getArtistReleaseData("10538149");
-
-
   async getDiscogData(url: string) {
-    // const data = httpClient.get(`${url}/artists/`);
-    // use promise.all to do mapping and get all the data I need. 
-    // then return it in the format I need
-    // only have to call useSWR one time in the page. 
-    // const getArtistReleaseData = (artistId: string) => {
-
-    // }
     const artistReleaseData = await Promise.all(Object.values(ArtistIds).map(async (artistId) => {
       const releases = await httpClient.get(`${url}artists/${artistId}/releases`);
       const artistDetails = await httpClient.get(`${url}artists/${artistId}`)
-      // // @ts-ignore
-      // console.log(releases)
-      // // @ts-ignore
-      // const releaseDetails = Promise.all(releases.releases.map(async (release) => {
-      //   const details = await httpClient.get(release.resource_url);
-      //   return details
-      // }))
-      // console.log(releaseDetails, "release deets")
       // @ts-ignore
       return { artistDetails, releases: releases.releases }
     }))
-
     return artistReleaseData;
   }
 }
-
-// need to return an array of 
-// objects containing info for each artist
-// for each artist need to 1. get list of releases
-// 2. get release details for each release
-// 
--- pages/discography.tsx
+++ pages/discography.tsx
@@ -4,28 +4,12 @@ import { DiscogService } from '../domain/services/discog-service';
 import List from '../components/list';
 
 export default function Discography(){
-  // need to use this combination of urls to get all the data needed from discogs.
-  // first get releases of mine, then get more data about those releases for specifcs about my role
-  // might be a good usecase for promise.all, but unsure why SWR doesn't use promises
-  // need to do a separate call to get Ricky Mirage releases
-  // todo: move this func to another section of the app to handle data getting
-  // todo: handle error
-  // https://api.discogs.com/artists/6067515
-  // https://api.discogs.com/artists/6067515/releases
 
   const discogService = new DiscogService();
 
   const baseUrl = "https://api.discogs.com/";
 
   const {data, error} = useSWR(baseUrl, discogService.getDiscogData);
-
-  // TODO:
-  // Loop over each artist,
-  // within the loop, pass the releases to a new LinkList component
-  // The List component will loop over the releases data
-  // utilize loading and error states from useSWR
-  // see here: https://swr.vercel.app/
-  // use id to get artist info -- name, etc. "Credited as Nick Harris, Credited as Ricky Mirage". 
 
   if (data) {
     return (
