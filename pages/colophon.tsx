34128fc1cb4b6a24bab04c0f5194fc1caf71d9cb
Nick Harris <nharris614@gmail.com>
2022-02-21 13:05:35 -0600

    Add colophon

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -1,5 +1,25 @@
 export default function Colophon() {
   return (
-    <div>info here about colophon. see spencer tweedy's site </div>
+    <>
+      <div>info here about colophon. see spencer tweedy's site </div>
+      <p>
+        Generator	Next.js
+        Host	Netlify
+        Git host	GitHub
+        Registrar	iWantMyName
+        Newsletter	Campaign Monitor
+        Anonymous analytics	Umami
+        Notes	Bear and Field Notes
+        Syntax	Markdown
+        Vector software	Sketch
+        Code editor	Nova
+        OBSERVATIONS is set in ISO by Scribble Tone. The logo is “Connections” and the list markers are a modified “Star” licensed from the Noun Project.
+
+        You can find all Observations posts in the archive.
+
+        Thank you!
+        Spencer
+      </p>
+    </>
   )
 }
