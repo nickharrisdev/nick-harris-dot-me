5c7078f2ac11d3a641a74140acbbfc230b319206
Nick Harris <nharris614@gmail.com>
2022-02-28 11:03:50 -0600

    fix speedlify link

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -28,7 +28,7 @@ export default function Colophon() {
        )
      })}
         <p className="mb-1 mt-4 font-bold">
-          <a href="https://621bf9b23270f900085f39e9--agitated-hoover-87d428.netlify.app/nickharris.me/" target="_blank" rel="noopener noreferrer">
+          <a href="https://agitated-hoover-87d428.netlify.app/nickharris.me/" target="_blank" rel="noopener noreferrer">
             Lighthouse scores and performance data
           </a>
         </p>
