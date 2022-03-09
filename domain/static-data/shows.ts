deae8dfecc72c54270c692a7102454664dfeb6d5
Nick Harris <nharris614@gmail.com>
2022-03-09 17:36:21 -0600

    Add some upcoming shows

--- domain/static-data/shows.ts
+++ domain/static-data/shows.ts
@@ -1,6 +1,8 @@
 import { Groups } from "../types/groups.enum";
 
 export default [
+  { venue: "Codfish Hollow Barnstormers (Iowa)", group: Groups.MOONER, date: new Date("07/09/2022"), link: "https://www.bandsintown.com/e/1021816142-mooner-at-codfish-hollow-barnstormers" },
+  { venue: "Lakeview Festival of the Arts", group: Groups.MOONER, date: new Date("09/10/2022"), link: "https://lakevieweastfestivalofthearts.com/" },
   { venue: "Golden Dagger", group: Groups.LK, date: new Date("03/11/2022"), link: "https://goldendagger.com/event-detail/11802305/lee-ketch-album-release-show/" },
   { venue: "Golden Dagger", group: Groups.YMIAH, date: new Date("03/22/2022"), link: "https://do312.com/events/2022/3/22/young-man-in-a-hurry-em-spel-tickets" },
   { venue: "Schuba's", group: Groups.YMIAH, date: new Date("02/10/2022"), link: "https://lh-st.com/shows/02-10-2022-young-man-in-a-hurry/" },
