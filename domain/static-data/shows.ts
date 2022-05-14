a9f15d1b61d284b9e041a3122e51b5730529fbd9
Nick Harris <nharris614@gmail.com>
2022-05-14 10:59:32 -0500

    Update shows

--- domain/static-data/shows.ts
+++ domain/static-data/shows.ts
@@ -1,11 +1,10 @@
 import { Groups } from "../types/groups.enum";
 
-// Todo possibilities -- add city/state? sort by year once more are added? 
+// sort by year once more are added? 
 
 export default [
   { city: "Elmhurst, IL", venue: "Elmhurst Hall", group: Groups.YMIAH, date: new Date("04/07/2022"), link: "https://elmhursthall.com/event/young-man-in-a-hurry/" },
   { city: "Chicago, IL", venue: "The Hideout", group: Groups.YMIAH, date: new Date("06/24/2022"), link: "https://hideoutchicago.com/event/young-man-in-a-hurry-%c2%b7-neptunes-core/" },
-  { city: "Maquoketa, IA", venue: "Codfish Hollow Barnstormers", group: Groups.MOONER, date: new Date("07/09/2022"), link: "https://www.bandsintown.com/e/1021816142-mooner-at-codfish-hollow-barnstormers" },
   { city: "Chicago, IL", venue: "Lakeview Festival of the Arts", group: Groups.MOONER, date: new Date("09/10/2022"), link: "https://lakevieweastfestivalofthearts.com/" },
   { city: "Chicago, IL", venue: "Golden Dagger", group: Groups.LK, date: new Date("03/11/2022"), link: "https://goldendagger.com/event-detail/11802305/lee-ketch-album-release-show/" },
   { city: "Chicago, IL", venue: "Golden Dagger", group: Groups.YMIAH, date: new Date("03/22/2022"), link: "https://do312.com/events/2022/3/22/young-man-in-a-hurry-em-spel-tickets" },
