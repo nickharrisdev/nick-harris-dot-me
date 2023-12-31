31fb3384b46f2e4a79ad7853b28c3a4f479a9403
Nick Harris <nharris614@gmail.com>
2023-12-31 16:41:02 -0600

    Update shows

--- domain/static-data/shows.ts
+++ domain/static-data/shows.ts
@@ -7,6 +7,11 @@ import { Show } from "../types/show.interface";
 // Add notes: {emoji: ""} to link to show notes
 
 const shows: Show[] = [
+  { city: "Chicago, IL", venue: "Schuba's", group: Groups.YMIAH, date: new Date("01/06/2024"), link: "https://lh-st.com/shows/01-06-2024-the-vulgar-boatmen/" },
+  { city: "Highwood, IL", venue: "Mojo's Vinyl Bar", group: Groups.YMIAH, date: new Date("12/02/2023"), link: "https://www.facebook.com/events/1570534920421911/" },
+  { city: "Glen Ellyn, IL", venue: "Saint Barnabas Episcopal Church", group: Groups.SteveSlagg, date: new Date("11/11/2023"), link: "https://barnabasencourager.org/upstairs-downstairs-benefit-concerts" },
+  { city: "Chicago, IL", venue: "Constellation", group: Groups.YMIAH, date: new Date("10/14/2023"), link: "https://constellation-chicago.com/seetickets-event/ymiah-with-jason-stein/" },
+  { city: "Chicago, IL", venue: "Voice (R)evolution", group: Groups.SteveSlagg, date: new Date("07/28/2023"), link: "https://www.voicerevolution.com/services" },
   { city: "Chicago, IL", venue: "Golden Dagger", group: Groups.MOONER, date: new Date("06/15/2023"), link: "https://bit.ly/3qrLDdM" },
   { city: "Chicago, IL", venue: "Burlington", group: Groups.SteveSlagg, date: new Date("02/23/2023"), link: "http://www.theburlingtonbar.com/" }, { city: "Chicago, IL", venue: "Burlington", group: Groups.YMIAH, date: new Date("11/11/2022"), link: "http://www.theburlingtonbar.com/" },
   { city: "Chicago, IL", venue: "A friend's wedding", group: Groups.MOONER, date: new Date("06/25/2022"), link: "https://en.wikipedia.org/wiki/Wedding" },
