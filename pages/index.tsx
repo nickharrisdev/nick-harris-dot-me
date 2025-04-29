7d6627aaaad5f376c531b281162bec2a1cafa894
Nick Harris <38137359+nickharrisdev@users.noreply.github.com>
2025-04-29 12:53:02 -0500

    Update bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import type { GetStaticProps } from 'next'
 import { getSortedProjectsData } from '../lib/projects'
 import { Projects } from '../domain/types/projects.interface'
 
+/**
+ * I'm a product-minded software engineer who believes the most important part of software is the people — both those who build it and those who use it. I see software as a medium for communicating intentions, reflecting human relationships, and solving real-world problems with empathy and purpose. Technology excites me most when it's used effectively to meet meaningful needs and bring people closer together.
+
+Specializing in front-end development, I create user interfaces that are beautiful, accessible, intuitive, and performant — for users — and reusable, extensible, and maintainable — for developers. I care deeply about building thoughtful tools that last, empower, and inspire.
+ */
 
 export default function Home({
   allProjectsData
@@ -12,10 +17,10 @@ export default function Home({
     <>
       <img src="/me.jpeg" alt="A picture of Nick Harris in a red hat." width="200" height="200" className="shadow-md rounded my-4"/>
       <p >
-        I&apos;m a Senior Software Engineer specializing in front-end development. I craft UIs that are beautiful, accessible, and performant for users while ensuring they are reusable, extensible, and maintainable for engineering teams.
+        I&apos;m a product-minded software engineer who believes the most important part of software is the people — both those who build it and those who use it. I see software as a medium for communicating intentions, reflecting human relationships, and solving real-world problems with empathy and purpose. Technology excites me most when it's used effectively to meet meaningful needs and bring people closer together.
       </p>
       <p> 
-        Beyond building great software, I&apos;m passionate about mentoring, improving developer experience, and contributing to technology that makes a meaningful impact. I also volunteer my expertise to support nonprofit organizations whose missions inspire me.
+       Specializing in front-end development, I create user interfaces that are beautiful, accessible, intuitive, and performant — for users — and reusable, extensible, and maintainable — for developers. I care deeply about building thoughtful tools that last, empower, and inspire.
       </p>
       <p> 
         When not making stuff on the internet, I can usually be found playing music with <a href="https://moonerband.com/" target="_blank" rel="noopener noreferrer"> Mooner</a>, <a href="https://youngmaninahurry.com" target="_blank" rel="noopener noreferrer">Young Man in a Hurry</a>, or attending a local show. 
