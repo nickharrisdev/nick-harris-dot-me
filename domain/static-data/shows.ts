b5310504f6697957b84f012f54d04345bd9ca75c
Nick Harris <38137359+nickharrisdev@users.noreply.github.com>
2025-08-08 19:13:42 -0500

    Update shows.ts

--- domain/static-data/shows.ts
+++ domain/static-data/shows.ts
@@ -9,6 +9,27 @@ import { Show } from "../types/show.interface";
 const shows: Show[] = [
   {
     city: "Chicago, IL",
+    venue: "Cafe Mustache",
+    group: Groups.SteveSlagg,
+    date: new Date("08/26/2025"),
+    link: "http://www.cafemustache.com/"
+  },
+  {
+    city: "Chicago, IL",
+    venue: "Rattleback Records",
+    group: Groups.LK,
+    date: new Date("08/03/2025"),
+    link: "https://rattlebackrecords.com/events/"
+  },
+  {
+    city: "Chicago, IL",
+    venue: "Rattleback Records",
+    group: Groups.LK,
+    date: new Date("08/03/2025"),
+    link: "https://rattlebackrecords.com/events/"
+  },
+  {
+    city: "Chicago, IL",
     venue: "Constellation",
     group: Groups.MOONER,
     date: new Date("05/15/2025"),
@@ -49,7 +70,7 @@ const shows: Show[] = [
     date: new Date("01/09/2025"),
     link: "https://www.seetickets.us/event/daniel-knox-with-lee-ketch/625413"
   },
-  { city: "Chicago, IL", venue: "Contorno", group: Groups.Contorno, date: new Date("01/05/2025"), link: "https://tonedeafrecs.com/" },
+  { city: "Chicago, IL", venue: "Tone Deaf Records", group: Groups.Contorno, date: new Date("01/05/2025"), link: "https://tonedeafrecs.com/" },
   { city: "Chicago, IL", venue: "California Clipper", group: Groups.SteveSlagg, date: new Date("11/16/2024"), link: "https://californiaclipper.com/" },
   { city: "Chicago, IL", venue: "The Hideout", group: Groups.LK, date: new Date("09/26/2024"), link: "https://hideoutchicago.com/events/?rhp_paged=1&rhp_event_display=list&view=list&rhp_bar_rhp_gen=0&rhp_bar_rhp_venue=0&rhp_bar_rhp_month=09" },
   { city: "Chicago, IL", venue: "Voice (R)evolution", group: Groups.SteveSlagg, date: new Date("09/21/2024"), link: "https://www.voicerevolution.com/services" },
